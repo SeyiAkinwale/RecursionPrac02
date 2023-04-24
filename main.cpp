58fc6af8822ef849e55427f07e9bdf9b024a12af
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-24 18:00:22 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+bool connectedPath(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec) 
+{
+
+return true;
+}
+
+int main() {
+  string simpleMaze[]={"XXX","X.X","XXX"};
+  cout<<connectedPath(simpleMaze, 3,3, 1,1, 1,1);
+}
