2c09043d3802dc23c82717ee283bd36a000de42b
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-04-25 00:13:04 +0000

    false same starting ending

--- .ccls-cache/@home@runner@RecursionPrac02/main.cpp
+++ .ccls-cache/@home@runner@RecursionPrac02/main.cpp
@@ -4,9 +4,13 @@ using namespace std;
 bool connectedPath(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec) 
 {
 if (er==sr && ec==er) return true;
+  else return false;
 }
 
-int main() {
-  string simpleMaze[]={"XXX","X.X","XXX"};
-  cout<<connectedPath(simpleMaze, 3,3, 1,1, 1,1);
+int main() 
+{
+  string baseMaze[]={"XXX","X.X","XXX"};
+  string threeSpotsRow[]={"XXXXX", "X...X", "XXXXX"};
+  cout<<connectedPath(baseMaze, 3,3, 1,1, 1,1)<<endl;
+  cout<<connectedPath(threeSpotsRow, 3,5, 1,1, 1,2);
 }
